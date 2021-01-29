72d47883991f74276ec31043f3c657c874cc57a3
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-01-30 02:01:00 +0900

    Initial commit

---
+++ app/src/main/java/app/Ito/Akki/firstapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.Ito.Akki.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener{
+            button.text = "クリックされました"
+        }
+    }
+}
